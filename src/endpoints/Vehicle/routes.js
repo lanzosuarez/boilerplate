539713db0ffe17e9fc25c39b142ae52bfad5ef8f
AV_MBP07032017 <av_mbp07032017@AV-MBP07032017s-MacBook.local>
2017-08-17 12:37:48 +0800

    Vehicle Information, Vehicle Transaction History, Vehicle Edit Information

--- src/endpoints/Vehicle/routes.js
+++ src/endpoints/Vehicle/routes.js
@@ -4,6 +4,7 @@ const { validateAppToken } = require('../../utils/security_utils');
 
 const get_vehicle_info_v1 = require('./_v/v1/vehicle_info');
 const get_vehicle_transaction_history_v1 = require('./_v/v1/vehicle_transaction_history');
+const vehicle_info_edit_v1 = require('./_v/v1/vehicle_info_edit');
 
 api.get({ path: '/jrc/vehicle_information' },
     get_vehicle_info_v1
@@ -11,4 +12,8 @@ api.get({ path: '/jrc/vehicle_information' },
 
 api.get({ path: '/jrc/vehicle_transaction_history' },
     get_vehicle_transaction_history_v1
+);
+
+api.patch({ path: '/jrc/vehicle_info_edit'},
+    vehicle_info_edit_v1
 );
